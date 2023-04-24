1fd28c05de364b7d14fd786a624d693142397a01
ALAN NATANIEL JOLODOVSKY <46830988@ort>
2023-04-24 10:13:10 -0300

    2 funciones nuevas

--- Program.cs
+++ Program.cs
@@ -7,5 +7,22 @@ class Program
     {
         Dictionary<int, cliente> Inscripciones= new Dictionary<int, cliente>();
         Console.WriteLine("i. Nueva Inscripción \nii. Obtener Estadísticas Del Evento \niii. Buscar Cliente \niv. Cambiar Entrada De Un Cliente \nv. Salir\n(Ingresar opción con numeros del teclado, no en números romanos)");
+
+    }
+    static int IngresarEntero(string mensaje)
+    {
+        Console.WriteLine(mensaje);
+       int entero = int.Parse(Console.ReadLine());
+        return entero;
+    }
+    static int IngresarEnteroEnRango(string mensaje, int minimo, int maximo)
+    {
+        int entero;
+        entero = IngresarEntero(mensaje);
+        while (entero < minimo || entero > maximo)
+        {
+            entero = IngresarEntero("---Error---Número inválido--- " + mensaje);
+        }
+        return entero;
     }
 }
